41e4a383a011b810dbafe42a5afd226ed748c1e7
Eddie Gomez <eddieegomez4@gmail.com>
2019-07-21 13:39:59 -0700

    refactoring contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,21 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 class ContactPage extends React.Component {
   state = {
     firstName: "",
     lastName: "",
+    email: "",
+    message: "",
   }
 
-  handleInputChange = event => {
+  handleChange = event => {
     const target = event.target
     const value = target.value
     const name = target.name
@@ -17,57 +25,76 @@ class ContactPage extends React.Component {
       [name]: value,
     })
   }
-  handleSubmit = event => {
-    // event.preventDefault()
-    console.log(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+
+    e.preventDefault()
   }
 
   render() {
+    const { firstName, lastName, email, message } = this.state
     console.log(this.state)
-
     return (
       <Layout>
         <SEO title="Contact" />
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          // netlify-honeypot="bot-field"
-          // onSubmit={this.handleSubmit}
-          style={{
-            border: "1px solid red",
-            position: "relative",
-            top: "200px",
-          }}
-        >
-          <input type="hidden" name="bot-field" />
-          <input type="hidden" name="form-name" value="contact" />
-          <label>
-            First name
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleInputChange}
-              placeholder="Bob"
-            />
-          </label>
-          <label>
-            Last name
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleInputChange}
-              placeholder="Dylan"
-            />
-          </label>
-          <label>
-            Email
-            <input name="email" placeholder="cool@emai.com" type="email" />
-          </label>
-          <textarea name="message" />
-          <button type="submit">Submit</button>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              First Name:{" "}
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={this.handleChange}
+                placeholder="Bob"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Last Name:{" "}
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={this.handleChange}
+                placeholder="Dylan"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email:{" "}
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                placeholder="cool@email.com"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:{" "}
+              <textarea
+                name="message"
+                value={message}
+                onChange={this.handleChange}
+                placeholder="Howdy!"
+              />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </Layout>
     )
